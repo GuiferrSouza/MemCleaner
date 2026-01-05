02d5785f4304196947ba6687f69ddfc4ad2b2604
Guilherme Ferreira de Souza <=>
2026-01-05 14:51:50 -0300

    Initial commit - Memmory cleaner using native dlls

---
+++ MemCleaner/Program.cs
@@ -0,0 +1,23 @@
+﻿using MemCleaner;
+using System;
+
+public static class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Status antes da limpeza:");
+        MemoryCleaner.ShowMemoryStatus();
+
+        Console.WriteLine("\nLimpando memória do sistema...");
+        MemoryCleaner.ClearSystemCache();
+
+        Console.WriteLine("Limpando processos...");
+        MemoryCleaner.CleanAllProcesses();
+
+        Console.WriteLine("\nStatus após limpeza:");
+        MemoryCleaner.ShowMemoryStatus();
+
+        Console.WriteLine("\nConcluído.");
+        Console.ReadKey();
+    }
+}
